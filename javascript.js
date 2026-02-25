c6150bc733f43b1528d343cea05c0ab4af589b8a
Raymond Lozano <70067176+ray-lozano@users.noreply.github.com>
2026-02-25 09:50:49 -0800

    Completed the logic for the computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+function getComputerChoice() {
+    // Randomly returns either rock, paper, or scissors
+    let random = Math.floor(Math.random() * 3);
+    
+    if (random == 0) {
+        console.log('Rock');
+        return 'Rock';
+    }
+    else if (random == 1) {
+        console.log('Paper');
+        return 'Paper';
+    }
+    else if (random == 2) {
+        console.log('Scissors');
+        return 'Scissors';
+    }
+}
+
+function getHumanChoice () {
+
+}
+
+getComputerChoice();
