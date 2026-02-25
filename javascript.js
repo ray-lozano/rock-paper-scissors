cbea7298dce980077a2dd85327cc6d3f7eb39630
Raymond Lozano <70067176+ray-lozano@users.noreply.github.com>
2026-02-25 10:12:12 -0800

    Completed the function for getting the human player choice for rock-paper-scissors

--- javascript.js
+++ javascript.js
@@ -3,21 +3,35 @@ function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
     
     if (random == 0) {
-        console.log('Rock');
         return 'Rock';
     }
     else if (random == 1) {
-        console.log('Paper');
         return 'Paper';
     }
     else if (random == 2) {
-        console.log('Scissors');
         return 'Scissors';
     }
 }
 
 function getHumanChoice () {
+    let choice = prompt('Pick rock, paper, or scissors: ');
+    console.log(choice);
 
+    // Check if the user entered a valid choice.
+    while (choice.toLowerCase() != 'rock' && choice.toLowerCase() != 'paper' && choice.toLowerCase() != 'scissors') {
+        choice = prompt('Please choose either rock, paper, or scissors: ');
+    }
+
+    if (choice.toLowerCase() == 'rock') {
+        return 'Rock';
+    }
+    else if (choice.toLowerCase() == 'paper') {
+        return 'Paper';
+    }
+    else if (choice.toLowerCase() == 'scissors') {
+        return 'Scissors';
+    }
 }
 
 getComputerChoice();
+getHumanChoice();
